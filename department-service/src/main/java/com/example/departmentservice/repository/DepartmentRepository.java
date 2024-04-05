b2664264eecb2f14ca7c43df430a4f5f33c667fd
Pham Thanh son (FIS PBDXGHN) <sonpt51@fpt.com>
2024-04-05 16:04:43 +0700

    delete department service and employee

--- department-service/src/main/java/com/example/departmentservice/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.departmentservice.repository;
-
-import com.example.departmentservice.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
