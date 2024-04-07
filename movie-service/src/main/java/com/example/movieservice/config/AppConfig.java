1c528c10b8a16d782f35bbf72893e355453044ef
Pham Thanh son (FIS PBDXGHN) <sonpt51@fpt.com>
2024-04-07 21:02:28 +0700

    form file service

---
+++ movie-service/src/main/java/com/example/movieservice/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.example.movieservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return restTemplate();
+    }
+
+}
