d105a52c89793fc445a7e6b883014ac9707936cd
Pham Thanh son (FIS PBDXGHN) <sonpt51@fpt.com>
2024-04-07 20:24:47 +0700

    db + entity movie service

---
+++ movie-service/src/main/java/com/example/movieservice/entity/movie_genre.java
@@ -0,0 +1,30 @@
+package com.example.movieservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class movie_genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_genre", referencedColumnName = "id")
+    private Genre genre;
+
+    @ManyToOne
+    @JoinColumn(name = "id_movie", referencedColumnName = "id")
+    private Movie movie;
+
+}
