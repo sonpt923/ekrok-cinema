a9ab28130bdc4adb934f1ef4ec52dc5afc0a5bd7
Pham Thanh son (FIS PBDXGHN) <sonpt51@fpt.com>
2024-04-08 15:16:19 +0700

    cinema

---
+++ cinema-service/src/main/java/com/example/cinemaservice/entity/Chair.java
@@ -0,0 +1,20 @@
+package com.example.cinemaservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "chair")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Chair {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+}
