bbf1ab0bd5dbdbe699b31f15cc0bea33f695c414
Pham Thanh son (FIS PBDXGHN) <sonpt51@fpt.com>
2024-04-15 20:23:03 +0700

    db cinema service

--- cinema-service/cinema.sql
+++ cinema-service/cinema.sql
@@ -1,1 +1,56 @@
-CREATE SCHEMA `cinema` ;
+CREATE SCHEMA `cinema`;
+
+CREATE TABLE `cinema`.`room`
+(
+    `id`           BIGINT       NOT NULL AUTO_INCREMENT,
+    `name`         VARCHAR(145) NOT NULL,
+    `total_chair`  BIGINT       NOT NULL,
+    `status`       BIGINT       NOT NULL,
+    `created_time` DATETIME     NOT NULL default NOW(),
+    `created_by`   VARCHAR(45)  NOT NULL,
+    `updated_time` DATETIME NULL,
+    `updated_by`   VARCHAR(45) NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE,
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+);
+
+CREATE TABLE `cinema`.`chair_type`
+(
+    `id`           BIGINT       NOT NULL AUTO_INCREMENT,
+    `name`         VARCHAR(145) NOT NULL,
+    `status`       BIGINT       NOT NULL,
+    `created_time` DATETIME     NOT NULL default NOW(),
+    `created_by`   VARCHAR(45)  NOT NULL,
+    `updated_time` DATETIME NULL,
+    `updated_by`   VARCHAR(45) NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE,
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
+);
+
+CREATE TABLE `cinema`.`chair`
+(
+    `id`            BIGINT      NOT NULL AUTO_INCREMENT,
+    `id_room`       BIGINT      NOT NULL,
+    `id_chair_type` BIGINT      NOT NULL,
+    `name`          VARCHAR(45) NOT NULL,
+    `status`        BIGINT      NOT NULL,
+    `created_by`    VARCHAR(45) NOT NULL,
+    `created_time`  DATETIME    NOT NULL,
+    `updated_by`    VARCHAR(45) NULL,
+    `updated_time`  DATETIME NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+    INDEX           `FK_CHAIR_CHAIRTYPE_idx` (`id_chair_type` ASC) VISIBLE,
+    CONSTRAINT `FK_CHAIR_ROOM`
+        FOREIGN KEY (`id_room`)
+            REFERENCES `cinema`.`room` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
+    CONSTRAINT `FK_CHAIR_CHAIRTYPE`
+        FOREIGN KEY (`id_chair_type`)
+            REFERENCES `cinema`.`chair_type` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+);
--- user-service/db.sql
+++ user-service/db.sql
@@ -1,30 +1,13 @@
 CREATE SCHEMA `user`;
 
-CREATE TABLE `user`.`ranks`
-(
-    `id`           BIGINT      NOT NULL AUTO_INCREMENT,
-    `code`         VARCHAR(45) NOT NULL,
-    `name`         VARCHAR(45) NOT NULL,
-    `point`        BIGINT NOT NULL,
-    `created_by`   VARCHAR(45) NOT NULL,
-    `created_time` DATETIME    NOT NULL DEFAULT NOW(),
-    `updated_by`   VARCHAR(45) NULL,
-    `updated_time` DATETIME NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-    UNIQUE INDEX `code_UNIQUE` (`code` ASC) VISIBLE,
-    UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE
-);
-
 CREATE TABLE `user`.`user`
 (
     `id`           BIGINT       NOT NULL AUTO_INCREMENT,
-    `id_rank`      BIGINT       NOT NULL,
     `code`         VARCHAR(45)  NOT NULL,
     `name`         VARCHAR(145) NOT NULL,
     `username`     VARCHAR(45)  NOT NULL,
     `email`        VARCHAR(145) NOT NULL,
-    `role`         BIGINT  NOT NULL,
+    `role`         BIGINT       NOT NULL,
     `password`     VARCHAR(145) NOT NULL,
     `created_by`   VARCHAR(45)  NOT NULL,
     `created_time` DATETIME     NOT NULL DEFAULT NOW(),
@@ -37,10 +20,4 @@ CREATE TABLE `user`.`user`
     UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
     UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
     UNIQUE INDEX `password_UNIQUE` (`password` ASC) VISIBLE,
-    INDEX          `FK_USER_RANK_idx` (`id_rank` ASC) VISIBLE,
-    CONSTRAINT `FK_USER_RANK`
-        FOREIGN KEY (`id_rank`)
-            REFERENCES `user`.`ranks` (`id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
 );
